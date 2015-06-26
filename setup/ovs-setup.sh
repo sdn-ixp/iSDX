045c941f161ef95553132695d499c6c494b2f15e
rbirkner <ridix89@gmail.com>
2015-06-26 14:47:11 +0200

    changed to openvswitch 2.3.2

--- setup/ovs-setup.sh
+++ setup/ovs-setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 cd ~
-wget http://openvswitch.org/releases/openvswitch-2.0.1.tar.gz
-tar xf openvswitch-2.0.1.tar.gz
-pushd openvswitch-2.0.1
+wget http://openvswitch.org/releases/openvswitch-2.3.2.tar.gz
+tar xf openvswitch-2.3.2.tar.gz
+pushd openvswitch-2.3.2
 DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary
 popd
 sudo dpkg -i openvswitch-common*.deb openvswitch-datapath-dkms*.deb python-openvswitch*.deb openvswitch-pki*.deb openvswitch-switch*.deb
