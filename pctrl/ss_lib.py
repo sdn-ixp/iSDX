06956bbb07257409b4714bb4e2380921cc7caffe
robertmacdavid <robertmacdavid@gmail.com>
2015-08-05 12:18:32 -0400

    restructured ss vmac code

---
+++ pctrl/ss_lib.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+#  Author:
+#  Rudiger Birkner (Networked Systems Group ETH Zurich)
+
+from netaddr import *
+
+
+def bitsRequired(supersets):
+    """ How many bits are needed to represent any set in this construction?
+    """
+    if supersets is None:
+    	return 0
+
+    logM = 0
+    if len(supersets) > 1:
+        logM = math.ceil(math.log(len(supersets) - 1, 2))
+    maxS = max(len(superset) for superset in supersets)
+
+    return int(logM + maxS)
+
+
+
+def rulesRequired(supersets, rulecounts):
+    """ How many rules will be needed by this superset construction?
+    """
+    if supersets is None:
+    	return 0
+
+    total = 0
+    for superset in supersets:
+        for part in superset:
+            total += rulecounts[part]
+    return total
+
+
+
+def minimize_ss_rules_greedy(self, peerSets, ruleCounts):
+    """ Given a list of supersets and the number of rules needed regarding
+        each participant in an outbound policy, greedily minimize
+        the number of rules that will result from the superset grouping.
+    """
+
+    # defensive copy
+    peerSets = [set(peerSet) for peerSet in peerSets]
+    # smallest sets first
+    peerSets.sort(key=len, reverse=True)
+    # how many bits are allowed?
+    max_bits = self.max_initial_bits
+
+
+    # the longest superset determines the current mask size
+    maxLength = max([len(prefix) for prefix in peerSets])
+
+    while (len(peerSets) > 1):
+        m = len(peerSets)
+
+        bits = self.bitsRequired()
+
+        # if M is 1 + 2^X for some X, logM will decrease after a merge
+        if ((m - 1) & (m - 2)) == 0:
+            bits = bits - 1
+
+
+        # bestSet1 and bestSet2 are our top choices for merging
+        bestImpact = 0
+        bestSet1 = None
+        bestSet2 = None
+
+        # for every pair of sets
+        for set1 in peerSets:
+            for set2 in peerSets:
+                if (set1 == set2):
+                    continue
+                
+                unionSize = len(set1.union(set2))
+
+                # if the merge would cause us to exceed the bit limit
+                if bits + max(0, unionSize - maxLength) > max_bits:
+                    continue
+
+
+                # choose the pair with the biggest impact on rulecount
+                impact = 0
+                for part in set1.intersection(set2):
+                    impact += ruleCounts[part]
+                
+                if (impact > bestImpact):
+                    bestImpact = impact
+                    bestSet1 = set1
+                    bestSet2 = set2
+
+        # if the best change is an increase, break
+        if (bestImpact == 0):
+            break
+        # merge the two best sets
+        bestSet1.update(bestSet2)
+        peerSets.remove(bestSet2)
+        # update the mask size if necessary
+        maxLength = max(len(bestSet1), maxLength)
+
+    return peerSets
+
+
+
+def is_subset_of_superset(subset, supersets):
+    for superset in supersets:
+        if ((set(superset)).issuperset(subset)):
+            return True
+    return False
+
+
+
+def removeSubsets(sets):
+    """ Removes all subsets from a list of sets.
+    """
+    final_answer = []
+
+    # defensive copy
+    sets = [set(_set) for _set in sets]
+    sets.sort(key=len, reverse=True)
+    i = 0
+    while i < len(sets):
+        final_answer.append(sets[i])
+
+        for j in reversed(range(i+1, len(sets))):
+            if sets[j].issubset(sets[i]):
+                del sets[j]
+
+        i += 1
+
+    return final_answer
+
+
+
+def clear_inactive_parts(prefixSets, activePeers):
+	for group in prefixSets:
+		group.intersection_update(activePeers)
+	return prefixSets
+
+
+#                
+### VMAC AND VMAC MASK BUILDERS
+#
+
+# constructs a match VMAC for checking reachability 
+def vmac_participant_match(superset_id, participant_index, sdx):
+    
+    # add superset identifier
+    vmac_bitstring = '{num:0{width}b}'.format(num=int(superset_id), width=(sdx.superset_id_size))
+        
+    # set bit of participant
+    vmac_bitstring += '{num:0{width}b}'.format(num=1, width=(participant_index+1))
+    vmac_bitstring += '{num:0{width}b}'.format(num=0, width=(sdx.VMAC_size-len(vmac_bitstring)))
+
+    # convert bitstring to hexstring and then to a mac address
+    vmac_addr = '{num:0{width}x}'.format(num=int(vmac_bitstring,2), width=sdx.VMAC_size/4)
+    vmac_addr = ':'.join([vmac_addr[i]+vmac_addr[i+1] for i in range(0,sdx.VMAC_size/4,2)])
+        
+    return vmac_addr
+
+# constructs the accompanying mask for reachability checks
+def vmac_participant_mask(participand_index, sdx):
+    # a superset which is all 1's
+    superset_bits = (1 << sdx.superset_id_size) - 1
+
+    return vmac_participant_match(superset_bits, participant_index, sdx)
+
+
+# constructs a match VMAC for checking next-hop
+def vmac_next_hop_match(participant_name, sdx, inbound_bit = False):
+        
+    # add participant identifier
+    vmac_bitstring = '{num:0{width}b}'.format(num=participant_name, width=(sdx.VMAC_size))
+
+    # set the 'inbound policy required' bit
+    if inbound_bit:
+        vmac_bitstring = '1' + vmac_bitstring[1:]
+
+    # convert bitstring to hexstring and then to a mac address
+    vmac_addr = '{num:0{width}x}'.format(num=int(vmac_bitstring,2), width=sdx.VMAC_size/4)
+    vmac_addr = ':'.join([vmac_addr[i]+vmac_addr[i+1] for i in range(0,sdx.VMAC_size/4,2)])
+            
+    return vmac_addr
+
+# returns a mask on just participant bits
+def vmac_next_hop_mask(sdx, inbound_bit = False):
+    part_bits_only = (1 << sdx.best_path_size) - 1
+
+    bitmask = vmac_best_path_match(part_bits_only, sdx, inbound_bit)
+
+    return bitmask
+
+
+# constructs stage-2 VMACs (for both matching and assignment)
+def vmac_part_port_match(participant_name, port_num, sdx, inbound_bit = False):
+    part_bits = sdx.best_path_size
+    remainder = sdx.VMAC_size - part_bits
+
+    # padding and port identifier on the left
+    vmac_bitstring_part1 = '{num:0{width}b}'.format(num=port_num, width=remainder)
+    # participant identifier on the right
+    vmac_bitstring_part2 = '{num:0{width}b}'.format(num=participant_name, width=part_bits)
+    # combined
+    vmac_bitstring = vmac_bitstring_part1 + vmac_bitstring_part2
+
+    # set the 'inbound policy required' bit
+    if inbound_bit:
+        vmac_bitstring = '1' + vmac_bitstring[1:]
+
+    # convert bitstring to hexstring and then to a mac address
+    vmac_addr = '{num:0{width}x}'.format(num=int(vmac_bitstring,2), width=sdx.VMAC_size/4)
+    vmac_addr = ':'.join([vmac_addr[i]+vmac_addr[i+1] for i in range(0,sdx.VMAC_size/4,2)])
+
+    return vmac_addr
+
+
+# returns a mask on participant and port bits
+def vmac_part_port_mask(sdx, inbound_bit = False):
+    part_port_size = sdx.best_path_size + sdx.port_size
+    part_port_bits = (1 << part_port_size) - 1
+
+    bitmask = vmac_best_path_match(part_port_bits, sdx, inbound_bit)
+
+    return bitmask
+
+# looks like 100000000000000
+def vmac_only_first_bit(sdx):
+
+    # return a match on participant 0 with inbound bit set to 1
+    return vmac_next_hop_match(0, sdx, inbound_bit=True)
+
+
+
+
+
+
---
+++ pctrl/supersets2.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+#  Author:
+#  Rudiger Birkner (Networked Systems Group ETH Zurich)
+
+from bgp_interface import *
+from ss_lib import *
+
+LOG = False
+
+class SuperSets():
+    def __init__(self, sdx, participant_name, config_file=None):
+        self.max_bits = 26
+        self.max_initial_bits = 22
+        self.best_path_size = 16
+        self.VMAC_size = 48
+        if config_file is not None:
+            with open(config_file, 'r') as f:
+                config = json.load(f)
+                self.max_bits =         config["max_bits"]
+                self.max_initial_bits = config["max_initial_bits"]
+                self.best_path_size =   config["best_path_size"]
+                self.VMAC_size =        config["vmac_size"]
+
+        # this is decided each time a recomputation occurs
+        self.mask_size = 0
+        self.supersets = []
+
+        self.sdx = sdx
+        self.participant_name = participant_name
+
+        self.rulecounts = {}
+        self.recompute_rulecounts()
+
+
+
+	def recompute_rulecounts(self):
+		self.rulecounts = {}
+        # construct the participant weight matrix
+        participant = self.sdx.participants[self.participant_name]
+        if ('outbound' in participant['policies']):
+        	policies = participant['policies']['outbound']
+        	for policy in policies:
+        		if ('fwd' in policy['action']):
+
+        			fwd_part = int(policy['action']['fwd'])
+        			if fwd_part not in self.rulecounts:
+        				self.rulecounts[fwd_part] = 0
+    				self.rulecounts[fwd_part] += 1
+
+
+
+    def update_supersets(self, updates):
+        sdx_msgs = {"type": "update",
+                    "changes": []}
+
+        self.recompute_rulecounts()
+
+        for update in updates:
+            if ('announce' not in update):
+            	continue
+            prefix = update['announce']['prefix']
+
+            # get set of all participants advertising that prefix
+            new_set = get_all_participants_advertising(prefix, self.sdx.participants)
+            # clean out the inactive participants
+            new_set.intersection_update(set(rulecounts.keys()))
+
+            # if the prefix group is still a subset, no update needed
+            if is_subset_of_superset(new_set, self.supersets):
+            	continue
+
+        	# else, find the best superset to merge
+            bestSuperset = None
+            bestCost = 1000000
+            
+            for superset in self.supersets:
+                # if this merge would exceed the current mask size limit, skip it
+                if len(new_set.union(superset)) > self.mask_size:
+                    continue
+
+                # the rule increase is the sum of all rules that involve each part added to the superset
+                cost = sum(ruleWeights[part] for part in new_set.difference(superset))
+                if cost < bestCost:
+                    bestCost = cost
+                    bestSuperset = superset
+
+            # if no merge is possible, recompute from scratch
+            if bestSuperset == None:
+                self.recompute_all_supersets()
+
+                sdx_msgs = {"type": "new",
+                            "changes": []}
+
+                for superset in self.supersets:
+                    for participant in superset:
+                        sdx_msgs["changes"].append({"participant_id": participant,
+                                                   "superset": superset_index,
+                                                   "position": self.supersets[superset_index].index(participant)})
+
+
+
+            # if merge is possible, do the merge and add the new rules required
+            else:
+            	superset_index = self.supersets.index(bestSuperset)
+            	new_members = list(new_set.difference(bestSuperset))
+            	bestSuperset.append(new_members)
+
+            	for participant in new_members:
+            		sdx_msgs["changes"].append({"participant_id": participant,
+                                                   "superset": superset_index,
+                                                   "position": self.supersets[superset_index].index(participant)})
+
+
+
+        # check which participants joined a new superset and communicate to the SDX controller
+        return sdx_msgs
+
+
+    def recomputation(self):
+    	self.recompute_rulecounts()
+        # get all sets of participants advertising the same prefix
+        peer_sets = get_all_participant_sets(self)
+        peer_sets = clear_inactive_parts(peer_sets)
+        peer_sets = removeSubsets(peer_sets)
+
+        self.supersets = self.minimize_rules_greedy(peer_sets)
+
+        # impose an ordering on each superset by converting sets to lists
+        for i in range(len(self.supersets)):
+        	self.supersets[i] = list(self.supersets[i])
+
+        # fix the mask size after a recomputation event
+        self.mask_size = self.max_bits
+        if len(self.supersets) > 1:
+            self.mask_size -= math.ceil(math.log(len(self.supersets)-1, 2))
+
+
+
+
+
+
