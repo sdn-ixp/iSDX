0b8642a04f263d9608136d6223811dbf930928a1
robertmacdavid <robertmacdavid@gmail.com>
2015-09-06 14:14:12 -0400

    forgot to add docs to generate_script.py

--- pctrl/generate_script.py
+++ pctrl/generate_script.py
@@ -4,15 +4,19 @@ import argparse
 
 LOG = True
 
-
+''' 
+This code takes in an SDX example directory, grabs all the participant IDs 
+in the examples config file, and generates a shell script which simply
+launches a participant controller instance in the background for each 
+participant ID.
+'''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('dir', help='the directory of the example')
     parser.add_argument('script', help='the name of the output pctrl script')
     args = parser.parse_args()
 
-    # locate config file
-    # TODO: Separate the config files for each participant
+
     base_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 "..","examples",args.dir,"config"))
     config_file = os.path.join(base_path, "sdx_global.cfg")
@@ -38,4 +42,3 @@ if __name__ == '__main__':
 
 
 
-    		
