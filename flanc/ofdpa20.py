1ea1dda059378bc88006eb1053be82cea2da76d1
Rick Porter <fporter@appcomsci.com>
2016-01-05 11:11:32 -0800

    l2 group code refactoring

--- flanc/ofdpa20.py
+++ flanc/ofdpa20.py
@@ -9,18 +9,21 @@ class OFDPA20():
     def __init__(self, config, origin):
         self.__dict__ = self.__shared_state # makes this a singleton
 
-        self.logger = logging.getLogger('OFDPA20')
-
         self.config = config
         self.origin = origin
 
-        self.known_l2_ifcs = set()       # set of port #'s
-        self.known_l2_rewrites = {}    # mapping from src/dst rewrite string to unique ID
-        self.l2_rewrite_uniq = 0
-        self.known_l2_multicast = {}  # mapping from mcast tuple of port #'s to unique ID
-        self.l2_multicast_uniq = 0
+        self.logger = logging.getLogger('OFDPA20')
 
         self.vlan = 1                    # untagged inputs go on vlan 1
+
+        self.l2_rewrite_to_gid = {}    # mapping from src/dst rewrite string to unique ID
+        self.l2_rewrite_uniq = 0
+
+        self.l2_multicast_to_gid = {}  # mapping from mcast tuple of port #'s to unique ID
+        self.l2_multicast_uniq = 0
+
+        self.gid_to_group_mod = {}
+        self.installed_group_mods = set()
 
     def get_table_id(self):
         return 60
@@ -68,55 +71,54 @@ class OFDPA20():
             self.logger.error("Unreachable code (I thought)!")
 
         instructions = [fm.parser.OFPInstructionActions(self.config.ofproto.OFPIT_APPLY_ACTIONS, group_actions)]
+        print "FDP gids: " + str(list(self.gid_to_group_mod))
         return (instructions, group_mods)
 
+    def make_group_mod(self, fm, datapath, gid, actions):
+        buckets = [fm.parser.OFPBucket(actions=actions)]
+        group_mod = fm.parser.OFPGroupMod(datapath=datapath,
+                                       command=self.config.ofproto.OFPGC_ADD,
+                                       type_=self.config.ofproto.OFPGT_INDIRECT,
+                                       group_id=gid,
+                                       buckets=buckets)
+        self.gid_to_group_mod[gid] = group_mod
+        return group_mod
+
+    # L2 Interface Group stuff
     def make_l2_interface_group_mod(self, fm, port, datapath):
         actions = [fm.parser.OFPActionOutput(port),
                    fm.parser.OFPActionPopVlan()]
-        buckets = [fm.parser.OFPBucket(actions=actions)]
-        return fm.parser.OFPGroupMod(datapath=datapath,
-                                       command=self.config.ofproto.OFPGC_ADD,
-                                       type_=self.config.ofproto.OFPGT_INDIRECT,
-                                       group_id=self.l2_interface_group_id(port),
-                                       buckets=buckets)
+        return self.make_group_mod(fm, datapath, self.l2_interface_group_id(port), actions)
 
     def l2_interface_group_id(self, port):
         return (self.vlan << 16) + (port & 0xffff)
 
+    # L2 Multicast Group stuff
     def make_l2_multicast_group_mod(self, fm, ports, datapath):
         actions = []
         for port in ports:
             actions.append(fm.parser.OFPActionGroup(group_id=self.l2_interface_group_id(port)))
-        buckets = [fm.parser.OFPBucket(actions=actions)]
-        return fm.parser.OFPGroupMod(datapath=datapath,
-                                       command=self.config.ofproto.OFPGC_ADD,
-                                       type_=self.config.ofproto.OFPGT_INDIRECT,
-                                       group_id=self.l2_multicast_group_id(ports),
-                                       buckets=buckets)
+        return self.make_group_mod(fm, datapath, self.l2_multicast_group_id(ports), actions)
 
     def l2_multicast_group_id(self, ports):
         mcast_key = tuple(sorted(ports))
-        if not mcast_key in self.known_l2_multicast:
-            self.known_l2_multicast[mcast_key] = 0x30000000 | (self.vlan << 16) | (self.l2_multicast_uniq & 0xffff)
+        if not mcast_key in self.l2_multicast_to_gid:
+            self.l2_multicast_to_gid[mcast_key] = 0x30000000 | (self.vlan << 16) | (self.l2_multicast_uniq & 0xffff)
             self.l2_multicast_uniq += 1
-        return self.known_l2_multicast[mcast_key]
+        return self.l2_multicast_to_gid[mcast_key]
 
+    # L2 Rewrite Group stuff
     def make_l2_rewrite_group_mod(self, fm, port, datapath, eth_src, eth_dst):
         actions = [fm.parser.OFPActionGroup(group_id=self.l2_interface_group_id(port))]
         if eth_src:
             actions.append(fm.parser.OFPActionSetField(eth_src=eth_src))
         if eth_dst:
             actions.append(fm.parser.OFPActionSetField(eth_dst=eth_dst))
-        buckets = [fm.parser.OFPBucket(actions=actions)]
-        return fm.parser.OFPGroupMod(datapath=datapath,
-                                       command=self.config.ofproto.OFPGC_ADD,
-                                       type_=self.config.ofproto.OFPGT_INDIRECT,
-                                       group_id=self.l2_rewrite_group_id(eth_src, eth_dst),
-                                       buckets=buckets)
+        return self.make_group_mod(fm, datapath, self.l2_rewrite_group_id(eth_src, eth_dst), actions)
 
     def l2_rewrite_group_id(self, eth_src, eth_dst):
         rewrite_key = ('' if not eth_src else "eth_src: " + eth_src) + ('' if not eth_dst else " eth_dst: " + eth_dst)
-        if not rewrite_key in self.known_l2_rewrites:
-            self.known_l2_rewrites[rewrite_key] = (1 << 28) | (self.l2_rewrite_uniq & 0xffff)
+        if not rewrite_key in self.l2_rewrite_to_gid:
+            self.l2_rewrite_to_gid[rewrite_key] = (1 << 28) | (self.l2_rewrite_uniq & 0xffff)
             self.l2_rewrite_uniq += 1
-        return self.known_l2_rewrites[rewrite_key]
+        return self.l2_rewrite_to_gid[rewrite_key]
