7514c5677ee384dd9bea0fc7cb2fb7230330fedd
Rick Porter <fporter@appcomsci.com>
2016-02-23 22:48:26 -0500

    Minor fix

--- setup/basic-setup.sh
+++ setup/basic-setup.sh
@@ -20,7 +20,7 @@ sudo pip install pymongo
 sudo apt-get install -y ssh git emacs sshfs graphviz feh
 sudo apt-get install -y libstring-crc32-perl
 
-echo PATH=\$PATH:`dirname "$(readlink -f "$0")"`/../bin >> ~/.profile
+echo PATH=\$PATH:~/iSDX/bin >> ~/.profile
 
 if [ -z "$NO_MININET" ]; then
     # set up some shortcuts
