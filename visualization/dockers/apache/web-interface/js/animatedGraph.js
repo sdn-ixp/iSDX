2e2e622c03b76b608f8772feec1588d431574192
Rick Porter <fporter@appcomsci.com>
2016-04-25 15:46:14 -0400

    Improve arrow heads

--- visualization/dockers/apache/web-interface/js/animatedGraph.js
+++ visualization/dockers/apache/web-interface/js/animatedGraph.js
@@ -85,9 +85,10 @@ var graph;
             .attr("id", String)
             .attr("viewBox", "0 -5 10 10")
             .attr("refX", 5)
-            .attr("markerWidth", 6)
-            .attr("markerHeight", 6)
+            .attr("markerWidth", 20)
+            .attr("markerHeight", 20)
             .attr("orient", "auto")
+            .attr("markerUnits", "userSpaceOnUse")
           .append("svg:path")
             .attr("d", "M0,-5L10,0L0,5");
 
